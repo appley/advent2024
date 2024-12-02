4ddd716c2779ac99818fdbfdeb81665eab01584d
pomme <copacabana@gmail.com>
2024-12-02 11:17:06 -0800

    factoring out fn

--- 1202.py
+++ 1202.py
@@ -42,48 +42,6 @@ def is_safe(l, curr):
             return False
 
 
-# def is_safe(l, curr, invalid=0):
-#     print("checking", l)
-#     print("nums invalid", invalid)
-
-#     def is_valid(a, b):
-#         if abs(b - a) >= 1 and abs(b - a) <= 3:
-#             return True
-#         else:
-#             return False
-        
-#     def is_in_sequence(a, b, c):
-
-#         if a >= b and b >= c:
-#             return True
-#         elif a <= b and b <= c:
-#             return True
-#         else:
-#             return False
-        
-#     if invalid > 1:
-#         print("too many invalid", invalid)
-#         return False
-#     if len(l) == 1:
-#         return True
-#     else:
-#         prev = curr
-#         j = l[0]
-#         k = l[1]
-#         if not is_in_sequence(prev, j, k) or not is_valid(j, k):
-#             invalid += 1
-#             new_l = [*l[:1], *l[2:]]
-#             print(new_l)
-#             print("invalids incremented", prev, j, k, l)
-#             return is_safe(new_l, j, invalid)
-#         if prev <= j and j < k and is_valid(j, k):
-#             return is_safe(l[1:], j, invalid)
-#         elif prev >= j and j > k and is_valid(j, k):
-#             return is_safe(l[1:], j, invalid)
-#         else:
-#             return False
-
-
 def remove_element(i, l):
 
     return [*l[:i], *l[i+1:]]
@@ -99,23 +57,26 @@ def is_safe_iter(l):
                 return True
     return False
 
-def count_safe_reports(lists):
+
+def count_safe_reports(lists, func):
 
     total = 0
     
     for l in lists:
-        if is_safe_iter(l):
+
+        if func(l):
             total += 1
 
     return total
 
 
+# part 2
 with open(input, "r") as f:
     reports = parse_file_to_intlists(f)
-    print(count_safe_reports(reports))
+    print(count_safe_reports(reports, is_safe_iter))
+
 
 # print(count_safe_reports(TEST))
-
 # print(is_safe([7, 6, 4, 2, 1], 7))
 # print(is_safe_iter([1, 2, 7, 8, 9]))
 # print(is_safe([9, 7, 6, 2, 1], 9))

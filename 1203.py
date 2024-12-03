4a2e8d20e14dfa998312007fe1c4610f57550df0
pomme <copacabana@gmail.com>
2024-12-03 14:05:34 -0800

    clean code

--- 1203.py
+++ 1203.py
@@ -2,15 +2,11 @@ import re
 
 input = "input/1203.txt"
 
+# test strings
 TEST = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
 TEST2 = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
-# TEST3 = 
 
 PATTERN = "mul\((\d+),(\d+)\)"
-
-DO = "do()"
-DONT = "don't()"
-
 DO = "don't\(\).*?do\(\)"
 
 
@@ -34,20 +30,15 @@ def produce_mul_instructions(int_pairs):
         total += i[0]*i[1]
     
     return total
-
-
-# def stack_instruction()
     
 
-def parse_file_and_produce_instructions(f):
+def parse_input_and_produce_instructions(input):
 
     int_pairs = []
 
-    for line in f:
+    for i in input:
 
-        int_pairs.extend(find_int_pairs(line))
-
-        print(int_pairs)
+        int_pairs.extend(find_int_pairs(i))
     
     return produce_mul_instructions(int_pairs)
 
@@ -68,19 +59,10 @@ def split_dos(s):
 
 
 
-# print(produce_mul_instructions(find_int_pairs(TEST)))
-
-# with open (input, "r") as f:
-#     print(parse_file_and_produce_instructions(f))
-
-# with open (input, "r") as f:
-#     print(stringify_file(f))
-
-
-# print(
-#     produce_mul_instructions(
-#         find_int_pairs(parse_file_and_produce_instructions(split_dos(TEST2)))))
-
-
+# part 1
 with open (input, "r") as f:
-    print(parse_file_and_produce_instructions(split_dos(stringify_file(f))))
+    print(parse_input_and_produce_instructions(f))
+
+#part 2
+with open (input, "r") as f:
+    print(parse_input_and_produce_instructions(split_dos(stringify_file(f))))

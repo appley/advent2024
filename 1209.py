41524aa95e4326908621d33c027e179c9ecbcfbe
pomme <copacabana@gmail.com>
2024-12-11 11:54:27 -0800

    working on part two

--- 1209.py
+++ 1209.py
@@ -3,6 +3,9 @@ input = "input/1209.txt"
 TEST = "2333133121414131402"
 
 
+PAIRS = [(2, 0, False), (2, 9, True), (3, 7, True), (2, 4, True), (3, 3, True), (1, 2, True), (3, 1, True), (2, 0, True), (4, 5, False), (4, 6, False), (4, 8, False)]
+
+
 # 00...111...2...333.44.5555.6666.777.888899
 
 d = {0: 0, 1: 0, 5: 1, 6: 1, 7: 1, 11: 2, 15: 3, 16: 3, 17: 3, 19: 4, 20: 4, 22: 5, 23: 5, 24: 5, 25: 5, 2: 9, 3: 9, 4: 8, 8: 8, 9: 8, 10: 8, 12: 7, 13: 7, 14: 7, 18: 6, 21: 6, 26: 6}
@@ -18,27 +21,44 @@ def to_string(f):
     return s
 
 
-def to_disk_map(input):
+def get_last_index(l):
 
-    def loop_string(item, num):
-        l = []
-        for i in range(num):
-            l.append(item)
-        return l
+    if len(l) % 2 == 0:
+        return 0
+    return 1
+    
+
+def compact_by_index(input):
+
+    # produce list of nums to insert in reverse
+    nums_list = []
+    for i, j in enumerate(input):
+        if i%2 == 0:
+            nums_list.append(int(j))
+
+    print("nums list", nums_list)
 
     l = []
+    curr_last_index = len(nums_list) - 1
 
-    t = 0
+    nums_index = 0
+
     for i, j in enumerate(input):
-        
         int_j = int(j)
         if i%2 == 0:
-            k = loop_string(t, int_j)
-            t += 1
+            l.append((int_j, nums_index, False))
+            nums_index += 1
         else:
-            k = loop_string(".", int_j)
-        l += k
-
+            while curr_last_index >= 0:
+                curr_last_val = nums_list[curr_last_index]
+                print("checking index: ", curr_last_index, "item: ", curr_last_val)
+                if curr_last_val <= int_j:
+                    l.append((curr_last_val, curr_last_index, True))
+                    curr_last_index -= 1
+                else:
+                    curr_last_index -= 1
+            # l.append((int_j, False))
+    
     return l
 
 
@@ -72,12 +92,51 @@ def to_lists(input):
         else:
             curr_index = new_list_index
             for k in range(num_times_loop):
-                spaces.append(curr_index)
+                # spaces.append(curr_index)
                 curr_index += 1
         
         new_list_index += int(j)
 
     return (nums, spaces)
+
+
+def to_list_two(input):
+
+    def to_index_pairs(num_times_loop, start_index, curr_num):
+        d = {}
+        for i in range(num_times_loop):
+            d[start_index] = curr_num
+            start_index +=1
+
+        # print("returning", l)
+        return d
+
+    nums = {}
+    spaces = []
+    new_list_index = 0
+    num_index = 0
+
+    # (index, num)
+    #nums [(0, 0), (1, 0), (5, 1), (6, 1), (7, 1)]
+    for i, j in enumerate(input):
+
+        num_times_loop = int(j[0])
+
+        if i%2 == 0 or j[1] is True:
+            nums.update(to_index_pairs(num_times_loop, new_list_index, num_index))
+            # print("nums", nums)
+            num_index += 1
+
+        else:
+            curr_index = new_list_index
+            for k in range(num_times_loop):
+            #     spaces.append(curr_index)
+                curr_index += 1
+        
+        new_list_index += int(j[0])
+
+    return sorted(nums.items())
+
 
 
 def to_compact_dict(t):
@@ -103,15 +162,22 @@ def check_sum(d):
     return total
         
 
-print(check_sum(to_compact_dict(to_lists(TEST))))
+# print(check_sum(to_compact_dict(to_lists(TEST))))
 
     
 
 # print(to_compact_dict(to_lists(TEST)))
 
-with open(input, "r") as f:
+# with open(input, "r") as f:
 
-    print(check_sum(to_compact_dict(to_lists(to_string(f)))))
+#     print(check_sum(to_compact_dict(to_lists(to_string(f)))))
+
+
+# with open(input, "r") as f:
+
+# print(compact_by_index(TEST))
+print(to_list_two(PAIRS))
+
 
 
 
